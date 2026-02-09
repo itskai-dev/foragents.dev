39652d81ce3f3c97eef23a1326b4ccc78ec57fef
Kai <kai@itskai.dev>
2026-02-09 00:22:36 -0800

    feat: Terms of service and privacy policy

--- jest.setup.ts
+++ jest.setup.ts
@@ -1,1 +1,17 @@
 import '@testing-library/jest-dom';
+
+// Mock IntersectionObserver
+global.IntersectionObserver = class IntersectionObserver {
+  constructor() {}
+  disconnect() {}
+  observe() {}
+  takeRecords() {
+    return [];
+  }
+  unobserve() {}
+} as unknown as typeof IntersectionObserver;
+
+// Mock scrollIntoView (only in jsdom environment)
+if (typeof Element !== 'undefined') {
+  Element.prototype.scrollIntoView = jest.fn();
+}
